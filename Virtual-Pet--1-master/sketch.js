cd5aa423d2a94d9e008bd84c7add7bf663b45cff
krishna572 <70430764+krishna572@users.noreply.github.com>
2020-11-16 22:47:05 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var backgroundImg;
+var dog,dogImg,dogeating;
+var database, foodS, foodStock
+
+function preload()
+{
+  //load images here
+  backgroundImg = loadImage("images/background.png");
+}
+
+function setup() {
+  createCanvas(800, 700);
+  background = (backgroundImg);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
